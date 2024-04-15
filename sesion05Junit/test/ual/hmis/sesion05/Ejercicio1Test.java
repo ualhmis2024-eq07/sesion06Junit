c60cf920f0f021f9a7bc05c4d8c90797b0f30a8f
Anass El Jabiry <aek676@inlumine.ual.es>
2024-04-15 23:13:19 +0200

    cambio paquete ejercicio1

--- sesion05Junit/test/ual/hmis/sesion05/Ejercicio1Test.java
+++ sesion05Junit/test/ual/hmis/sesion05/Ejercicio1Test.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import ual.hmis.sesion05.Ejercicio1;
+import ual.hmis.sesion05.ejercicio1.Ejercicio1;
 
 
 class Ejercicio1Test {
