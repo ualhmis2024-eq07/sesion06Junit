e3dd82bca76f70ddb7ac1aed54deac6979a51a36
Anass El Jabiry <aek676@inlumine.ual.es>
2024-04-23 21:50:41 +0200

    anass done

--- sesion05Junit/test/ual/hmis/sesion05/ejercicio4/Ejercicio3Test.java
+++ sesion05Junit/test/ual/hmis/sesion05/ejercicio4/Ejercicio3Test.java
@@ -8,6 +8,7 @@ import java.util.stream.Stream;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 
@@ -25,7 +26,12 @@ class Ejercicio3Test {
 	}
 	
 	@ParameterizedTest(name = "{index} => mezclar {0} con {1} y sale {2}")
-	@MethodSource("mezclaProvider")
+	@CsvSource({"Arrays.asList(), 					Arrays.asList(), 					Arrays.asList()",
+	"Arrays.asList(), 					Arrays.asList(1, 3, 5, 7, 9), 		Arrays.asList(1, 3, 5, 7, 9)",
+	"Arrays.asList(0, 2, 4, 6, 8), 		Arrays.asList(), 					Arrays.asList(0, 2, 4, 6, 8)",
+	"Arrays.asList(0, 2, 4, 6, 8), 		Arrays.asList(1, 3, 5, 7, 9), 		Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)",
+	"Arrays.asList(0, 2, 4, 0, 2, 4), 	Arrays.asList(0, 3, 5, 1, 3, 5), 	Arrays.asList(0, 1, 2, 3, 4, 5)"
+	})
 	public void testMezclarConjuntosOrdenados(List<Integer> list1, List<Integer> list2, List<Integer> result){
 		MezclaLineal<Integer> mezclaLineal = new MezclaLineal<Integer>();
 		assertEquals(mezclaLineal.mezclarConjuntosOrdenados(list1, list2).toString(), result.toString());
