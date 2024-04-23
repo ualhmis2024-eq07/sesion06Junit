28959b8bdc2f96b102ba05ab1eaad47bf72ddae5
Anass El Jabiry <aek676@inlumine.ual.es>
2024-04-23 21:52:30 +0200

    anass done

--- sesion05Junit/test/ual/hmis/sesion05/ejercicio4/Ejercicio3Test.java
+++ sesion05Junit/test/ual/hmis/sesion05/ejercicio4/Ejercicio3Test.java
@@ -26,12 +26,7 @@ class Ejercicio3Test {
 	}
 	
 	@ParameterizedTest(name = "{index} => mezclar {0} con {1} y sale {2}")
-	@CsvSource({"Arrays.asList(), 					Arrays.asList(), 					Arrays.asList()",
-	"Arrays.asList(), 					Arrays.asList(1, 3, 5, 7, 9), 		Arrays.asList(1, 3, 5, 7, 9)",
-	"Arrays.asList(0, 2, 4, 6, 8), 		Arrays.asList(), 					Arrays.asList(0, 2, 4, 6, 8)",
-	"Arrays.asList(0, 2, 4, 6, 8), 		Arrays.asList(1, 3, 5, 7, 9), 		Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)",
-	"Arrays.asList(0, 2, 4, 0, 2, 4), 	Arrays.asList(0, 3, 5, 1, 3, 5), 	Arrays.asList(0, 1, 2, 3, 4, 5)"
-	})
+	@MethodSource("mezclaProvider")
 	public void testMezclarConjuntosOrdenados(List<Integer> list1, List<Integer> list2, List<Integer> result){
 		MezclaLineal<Integer> mezclaLineal = new MezclaLineal<Integer>();
 		assertEquals(mezclaLineal.mezclarConjuntosOrdenados(list1, list2).toString(), result.toString());
